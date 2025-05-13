ed7aebc72e215966e4bd8fd43ebd560fe75234e3
kiran <kiranmourya1996@gmail.com>
2025-05-13 15:23:26 +0530

    added response entity

--- src/main/java/com/example/order/controller/ProductController.java
+++ src/main/java/com/example/order/controller/ProductController.java
@@ -4,6 +4,8 @@ import com.example.order.dto.ProductDTO;
 import com.example.order.entity.Product;
 import com.example.order.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,19 +17,19 @@ public class ProductController {
    private ProductService productService;
 
    @PostMapping("/products")
-   public Product saveProduct(@RequestBody ProductDTO productDTO){
-       return productService.saveProduct(productDTO);
+   public ResponseEntity<Product> saveProduct(@RequestBody ProductDTO productDTO){
+       return new ResponseEntity<>(productService.saveProduct(productDTO), HttpStatus.CREATED);
    }
 
 
    @GetMapping("/product-details")
-    public List<ProductDTO> getAllProductDetails(){
-        return productService.getAllProduct();
+    public ResponseEntity<List<ProductDTO>> getAllProductDetails(){
+        return new ResponseEntity<>(productService.getAllProduct(),HttpStatus.OK);
     }
 
     @GetMapping("/product-details/{id}")
-    public ProductDTO getProductDetailsById(@PathVariable Integer id){
-        return productService.getProductDetailsById(id);
+    public ResponseEntity<ProductDTO> getProductDetailsById(@PathVariable Integer id){
+        return new ResponseEntity<>(productService.getProductDetailsById(id),HttpStatus.OK);
     }
 
 
--- src/test/java/com/example/ProductServiceApplicationTest.java
+++ src/test/java/com/example/ProductServiceApplicationTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.example;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -7,15 +7,13 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class ProductServiceApplicationTest extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public ProductServiceApplicationTest(String testName )
     {
         super( testName );
     }
@@ -25,7 +23,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( ProductServiceApplicationTest.class );
     }
 
     /**
