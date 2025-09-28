e28ee13e5fe932ce331c70886bf999f078d8f6b2
kiran <kiranmourya1996@gmail.com>
2025-09-28 19:47:50 +0530

    unit test

--- src/main/java/com/example/order/repository/ProductRepository.java
+++ src/main/java/com/example/order/repository/ProductRepository.java
@@ -8,7 +8,4 @@ import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
-
-
-    Optional<Product> findByP_descriptions(String nonExisting);
 }
--- src/test/java/com/example/order/ProductServiceApplicationTest.java
+++ src/test/java/com/example/order/ProductServiceApplicationTest.java
@@ -1,14 +1,30 @@
 package com.example.order;
 
+import com.example.order.repository.ProductRepository;
+import com.example.order.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 public class ProductServiceApplicationTest {
+    @Autowired
+    ApplicationContext applicationContext;
+
     @Test
     void testMainMethodRunsWithoutException() {
+        // Check that the application context is not null
+        assertThat(applicationContext).isNotNull();
         assertDoesNotThrow(() -> ProductServiceApplication.main(new String[]{}));
+
+        // Check that your beans are loaded
+        assertThat(applicationContext.getBean(ProductService.class)).isNotNull();
+        assertThat(applicationContext.getBean(ProductRepository.class)).isNotNull();
+
+        System.out.println("✅ ApplicationContext loaded and beans are available");
     }
 }
